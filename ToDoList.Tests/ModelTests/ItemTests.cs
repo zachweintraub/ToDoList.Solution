a016aa5c4f95841a7b27011de40dbfdd1907b3b1
Tessa Sullivan <tessa.sullivan@gmail.com>
2019-04-23 16:18:58 -0700

    Initial files created with boilerplate README

---
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+
+namespace ToDoList.Tests
+{
+  [TestClass]
+  public class ItemTest
+  {
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      Item newItem = new Item();
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+
+  }
+}
