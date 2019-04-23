6ab78d258f5f0023edb0c910c6edc1000fdab994
Tessa Sullivan <tessa.sullivan@gmail.com>
2019-04-23 16:28:08 -0700

    Tests for Item constructor and GetDescription included

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -6,11 +6,19 @@ namespace ToDoList.Tests
   [TestClass]
   public class ItemTest
   {
+    [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
     {
       Item newItem = new Item();
       Assert.AreEqual(typeof(Item), newItem.GetType());
     }
-
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+      string result = newItem.GetDescription();
+      Assert.AreEqual(description, result);
+    }
   }
 }
