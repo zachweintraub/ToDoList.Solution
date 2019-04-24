a33d887c3fd3c8a8ee424df2f0727699eb037d48
Kevin Garvey and Tessa Sullivan <kevin@>
2019-04-24 09:30:10 -0700

    Added some tests and created a console interface allowing user to add items to the list or view it

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,16 +1,33 @@
+using System;
+using System.Collections.Generic;
+
 namespace ToDoList.Models
 {
   public class Item
   {
       private string _description;
+      private static List<Item> _instances = new List<Item>{};
 
       public Item (string description)
       {
           _description = description;
+          _instances.Add(this);
       }
       public string GetDescription()
       {
           return _description;
+      }
+      public static List<Item> GetAll()
+      {
+          return _instances;
+      }
+      public static void ClearAll()
+      {
+          _instances.Clear();
+      }
+      public void SetDescription(string newDiscription)
+      {
+        _description = newDiscription;
       }
   }
 }
