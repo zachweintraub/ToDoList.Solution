3d19b1147233d32608f3eaec66bb871e12e48d46
Kevin Garvey and Zach Weintraub <zach@>
2019-04-29 16:43:02 -0700

    Add MVC structure and controller tests

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,45 +1,20 @@
-using System;
-using System.Collections.Generic;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
-namespace ToDoList.Models
+namespace ToDoList
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main()
-        {
-            Console.WriteLine("Welcome to the To Do List.");
-            userPrompt();         
-        }
-        public static void userPrompt()
-            {
-                Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View/Quit)");
-                string userInput = Console.ReadLine().ToLower();
-                if (userInput == "add")
-                {
-                    System.Console.WriteLine("Enter the item to add");
-                    string newItemStr = Console.ReadLine();
-                    Item newItem = new Item(newItemStr);
-                    System.Console.WriteLine("Your item: " + newItem.GetDescription() +  " has been added to your list.");
-                    userPrompt();
-                }
-                else if (userInput == "view")
-                {
-                    List<Item> myList = Item.GetAll();
-                    foreach(Item item in myList)
-                    {
-                        System.Console.WriteLine(item.GetDescription());
-                    }
-                    userPrompt();
-                }
-                else if (userInput == "quit")
-                {
-                    System.Console.WriteLine("Thanks for using To Do List (TM)");
-                }
-                else 
-                {
-                    System.Console.WriteLine("Command not recognized, please choose Add or View");
-                    userPrompt();
-                }
-            }
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
+
+      host.Run();
     }
+  }
 }
